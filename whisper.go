46af4d3aefabedd7cd70edf4cb34fb3bfb46fbbe
dschaub <dan@danschaub.com>
2017-10-03 16:10:05 -0400

    it works!

--- whisper.go
+++ whisper.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"go.mozilla.org/sops/decrypt"
 	"gopkg.in/yaml.v2"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -13,44 +15,50 @@ import (
 func main() {
 	// get options: -f <file name> <command>
 	file := flag.String("f", "", "path to SOPS file")
+	passThroughEnv := flag.Bool("p", false, "pass the current environment through")
 	flag.Parse()
 
-	command := flag.Args()
+	command := strings.Join(flag.Args(), " ")
 
-	fmt.Println("file = ", *file)
-	fmt.Println("command = ", strings.Join(command, " "))
+	log.Printf("file = %v", *file)
+	log.Printf("command = %v", command)
 
 	if *file == "" || len(command) == 0 {
-		fmt.Println("usage: whisper -f <file> <command>")
-		os.Exit(1)
+		log.Fatal("usage: whisper -f <file> <command>")
 	}
 
 	path, err := filepath.Abs(*file)
 
 	if err != nil {
-		fmt.Println("Couldn't determine absolute path to file", err)
-		os.Exit(1)
+		log.Fatalf("Couldn't determine absolute path to file: %v", err)
 	}
 
 	yamlBytes, err := decrypt.File(path, "yaml")
 
 	if err != nil {
-		fmt.Println("Couldn't decrypt file!")
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("Couldn't decrypt file: %v", err)
 	}
 
-	yamlMap := make(map[interface{}]interface{})
-	err = yaml.Unmarshal(yamlBytes, &yamlMap)
+	secrets := make(map[string]interface{})
+	err = yaml.Unmarshal(yamlBytes, &secrets)
 
 	if err != nil {
-		fmt.Println("Couldn't figure out those yaml bytes")
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("Couldn't unmarshal YAML bytes: %v", err)
 	}
 
-	fmt.Printf("%v", yamlMap)
+	cmd := exec.Command("sh", "-c", command)
 
-	// get all of the values under environment key
-	// pass them to os.Exec?
+	if *passThroughEnv {
+		cmd.Env = os.Environ()
+	}
+
+	environmentMap := secrets["environment"].(map[interface{}]interface{})
+	for k, v := range environmentMap {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%v", k, v))
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
